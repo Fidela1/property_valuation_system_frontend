8f16f97ed761aa9fd810aa78f22e94e7b4d22352
Fidela 1 <fidelatuyizere4@gmail.com>
2026-06-09 11:22:50 +0200

    fix: clean build with dynamic routes

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  
+   output: 'standalone', // Important for Vercel
+  trailingSlash: false,
 };
 
 export default nextConfig;
